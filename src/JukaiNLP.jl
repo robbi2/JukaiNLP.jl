229d59af6adebbce84066f5ff35fe160a2638089
masashi-y <yoshikawa.masashi.yh8@is.naist.jp>
2016-07-21 18:28:40 +0900

    merge

--- src/JukaiNLP.jl
+++ src/JukaiNLP.jl
@@ -1,25 +1,19 @@
 module JukaiNLP
 
 using Compat
-<<<<<<< HEAD
-# using Merlin
-=======
->>>>>>> 05ecda532b39d9fd369f877bfaf3ab5cba751852
+using Merlin
 using ProgressMeter
 
 export decode, train
 
-<<<<<<< HEAD
 include("iddict.jl")
 include("accuracy.jl")
 include("beamsearch.jl")
-=======
->>>>>>> 05ecda532b39d9fd369f877bfaf3ab5cba751852
 include("tokenization/Tokenization.jl")
 include("depparsing/DepParsing.jl")
 
-# using .Tokenization
-# export Tokenizer
+using .Tokenization
+export Tokenizer
 using .DepParsing
 export DepParser
 
--- src/depparsing/depparser.jl
+++ src/depparsing/depparser.jl
@@ -7,29 +7,9 @@ type DepParser
     model
 end
 
-<<<<<<< HEAD
 function DepParser(path::AbstractString, model)
     words = IdDict(path)
     tags = IdDict(AbstractString)
     labels = IdDict(AbstractString)
     DepParser(words, tags, labels, model)
 end
-
-=======
-using Compat
-using ProgressMeter
-using TransitionParser
-
-include("utils.jl")
-include("token.jl")
-include("arcstd.jl")
-include("perceptron.jl")
-include("training.jl")
-include("accuracy.jl")
-
-wordspath = "../../dict/en-word_nyt.dict"
-trainpath = "../../corpus/wsj_02-21.conll"
-testpath = "../../corpus/wsj_23.conll"
-
-#train()
->>>>>>> 05ecda532b39d9fd369f877bfaf3ab5cba751852
