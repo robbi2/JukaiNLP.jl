4bc81e76f91d44117f891f03b4c80bc7fc1c8aee
Hiroyuki Shindo <hshindo@outlook.com>
2016-07-27 06:31:10 +0900

    Update io

--- src/tokenization/io.jl
+++ src/tokenization/io.jl
@@ -1,23 +1,71 @@
-function readfile(path)
-    data = readtsv(path)
-    for lines in data
-        chars = Int[]
-        for line in lines
-            items = split(line, '\t')
-            ids = map(c -> get!(dict, c, length(dict)+1), items[2])
-            append!(chars, ids)
-            
-
-            append!(chars, items[2])
-            if items[2] == " "
-            end
+function readfile(path::String, dict::IdDict)
+    unk, space, lf = dict["UNKNOWN"], dict[" "], dict["LF"]
+    chars = Int[]
+    ranges = UnitRange{Int}[]
+    lines = open(readlines, path)
+    pos = 1
+    for line in lines
+        line = chomp(line)
+        if isempty(line)
+            continue
         end
-    end
+        items = split(line, '\t')
 
-    doc = split(data, '\n', keep=false)
-    doc
+        for c in items[11]
+            c == '_' && continue
+            if c == 'S'
+                push!(chars, space)
+            elseif c == 'N'
+                push!(chars, lf)
+            end
+            pos += 1
+        end
+
+        form = items[2]
+        for c in form
+            push!(chars, get(dict,string(c),unk))
+        end
+        push!(ranges, pos:pos+length(form)-1)
+        pos += length(form)
+    end
+    chars, ranges
 end
 
+function readfile2(path::String, dict::IdDict)
+    unk, space, lf = dict["UNKNOWN"], dict[" "], dict["LF"]
+    chars, tags = Int[], Int[]
+    lines = open(readlines, path)
+    for i = 1:length(lines)
+        line = chomp(lines[i])
+        if isempty(line)
+            tags[end] = Tagset.ES
+            continue
+        end
+        items = split(line, '\t')
+
+        for c in items[11]
+            if c == 'S'
+                cc = space
+            elseif c == 'N'
+                cc = lf
+            else
+                continue
+            end
+            push!(chars, cc)
+            push!(tags, Tagset.O)
+        end
+
+        form = items[2]
+        for c in form
+            push!(chars, get(dict,string(c),unk))
+            push!(tags, Tagset.I)
+        end
+        tags[end] = Tagset.ET
+    end
+    chars, tags
+end
+
+#=
 function readtsv(path)
     doc = Vector{Vector{AbstractString}}[]
     sent = Vector{AbstractString}[]
@@ -35,6 +83,7 @@ function readtsv(path)
     length(sent) > 0 && push!(doc, sent)
     doc
 end
+=#
 
 #=
 function readconll(t::Tokenizer, path)
@@ -57,10 +106,3 @@ function readconll(t::Tokenizer, path)
     data_x, data_y
 end
 =#
-function readdict(path)
-    dict = Dict()
-    for line in open(readlines, path)
-        get!(dict, chomp(line), length(dict)+1)
-    end
-    dict
-end
